4b4f8b9fad68f8ab476be6979d0ce998c74c8cbc
ziyad <ziyadmuhammad9696@gmail.com>
2024-10-01 21:37:58 +0530

    admin routes created

--- controller/admin/adminCtrl.js
+++ controller/admin/adminCtrl.js
@@ -1,5 +1,31 @@
 const adminPage = (req, res) => {
   res.render("adminSide/admin");
 };
+const products = (req,res)=>{
+  res.render("adminSide/products");
+}
+const orders = (req,res)=>{
+  res.render("adminSide/orders");
+}
+const customers = (req,res)=>{
+  res.render("adminSide/customers");
+}
+const categories = (req,res)=>{
+  res.render("adminSide/categories");
+}
+const reports= (req,res)=>{
+  res.render("adminSide/reports");
+}
+const settings = (req,res)=>{
+  res.render("adminSide/settings");
+}
 
-module.exports = adminPage;
+module.exports = {
+  adminPage,
+  products,
+  orders,
+  customers,
+  categories,
+  reports,
+  settings
+};
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const adminPage = require('../controller/admin/adminCtrl')
+const {
+    adminPage,
+    products,
+    orders,
+    customers,
+    categories,
+    reports,
+    settings,
+} = require('../controller/admin/adminCtrl')
 
 router.get("/dashboard", adminPage);
+router.get("/products", products);
+router.get("/orders", orders);
+router.get("/customers", customers);
+router.get("/categories", categories);
+router.get("/reports", reports);
+router.get("/settings", settings);
+
+
+
 
 module.exports = router;
